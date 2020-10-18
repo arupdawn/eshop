d42751e1b4eeae809cdd7fa14693b2b0fa3432bf
ArupDawn <arupchandradawn@gmail.com>
2020-10-18 23:21:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Orders from "./Orders";
+import Checkout from "./Checkout";
+import OrderSuccess from "./OrderSuccess";
+import ProductDetails from "./ProductDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header/>
+            <Orders/>
+          </Route>
+
+          <Route path="/checkout">
+            <Header/>
+            <Checkout/>
+          </Route>
+
+          <Route path="/ordersuccess">
+            <Header/>
+            <OrderSuccess/>
+          </Route>
+
+          <Route path="/productdetails">
+            <Header/>
+            <ProductDetails/>
+          </Route>
+
+          {/* Default route should be at the bottom -- below */}
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
